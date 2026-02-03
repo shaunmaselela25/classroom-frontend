04e024e52eef2d80b83e11a8c05ad32618300d7e
shaunmaselela25 <160026940+shaunmaselela25@users.noreply.github.com>
2026-02-03 13:39:27 +0000

    implement upload widget

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,12 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
 import Dashboard from "@/pages/dashboard.tsx";
-import { BookOpen, Home } from 'lucide-react';
+import { BookOpen, GraduationCap, Home } from 'lucide-react';
 import { Layout } from "@/components/refine-ui/layout/layout.tsx";
 import SubjectsList from '@/pages/subjects/list.tsx';
 import SubjectsCreate from '@/pages/subjects/create.tsx';
-
+import ClassesList from '@/pages/classes/list.tsx';
+import ClassesCreate from '@/pages/classes/create.tsx';
 
 function App() {
   return (
@@ -36,24 +37,28 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: 'zIVscj-Rk0LFc-5HsYvx',
               }}
-              resources={[
-                {
-                  name: 'dashboard', list: "/", meta: { label: 'Home', icon: <Home />}
-                },
-                { name: 'subjects', list: '/subjects', create: '/subjects/create', meta: {label: 'Subjects', icon: <BookOpen />}},
-               // {}
-              ]}
-            >
-
-              <Routes>
-                <Route element={<Layout><Outlet /></Layout>}>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route path="subjects">
-                    <Route index element={<SubjectsList />} />
-                    <Route path="create" element={<SubjectsCreate />} />
-                  </Route>
-                </Route>
-              </Routes>
+      resources={[
+        {
+          name: 'dashboard', list: "/", meta: { label: 'Home', icon: <Home />}
+        },
+        { name: 'subjects', list: '/subjects', create: '/subjects/create', meta: {label: 'Subjects', icon: <BookOpen />}},
+        { name: 'classes', list: '/classes', create: '/classes/create', meta: {label: 'Classes', icon: <GraduationCap />}},
+        //{}
+      ]}
+    >
+      <Routes>
+        <Route element={<Layout><Outlet /></Layout>}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="subjects">
+            <Route index element={<SubjectsList />} />
+            <Route path="create" element={<SubjectsCreate />} />
+          </Route>
+          <Route path="classes">
+            <Route index element={<ClassesList />} />
+            <Route path="create" element={<ClassesCreate />} />
+          </Route>
+        </Route>
+      </Routes>
               <Toaster />
               <RefineKbar />
               <UnsavedChangesNotifier />
